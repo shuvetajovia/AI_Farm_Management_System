4578ed9986663b60a8da5d8ef61d01713d990c5b
A Shuveta Jovi <shuvetajovi2006a@gmail.com>
2026-05-30 00:13:16 +0530

    Import HttpResponse in views.py

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, TemplateView, DetailView
 from django.contrib import messages
